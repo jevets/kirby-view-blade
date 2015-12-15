a3ce512618dc9efca8f247035cef23d5c7fe2563
stevejamesson <steve@evendev.com>
2015-12-15 07:28:37 -0500

    progressing

--- src/BladeView.php
+++ src/BladeView.php
@@ -2,8 +2,37 @@
 
 namespace KirbyCasts\Kirby;
 
-class BladeView
+use KirbyCasts\Kirby\View\ViewInterface;
+
+/**
+ * Enables Kirby to use Laravel's Blade template engine
+ * instead of default native PHP templates
+ *
+ * @uses Philo\Blade\Blade
+ */
+class BladeView implements ViewInterface
 {
-    public function __construct();
-    public function make();
+    /**
+     * Create an instance of the view
+     *
+     * @param string $viewsDir
+     * @param string $cacheDir
+     * @return void
+     */
+    public function __construct($viewsDir, $cacheDir = '')
+    {
+
+    }
+
+    /**
+     * Get a rendered view as a string
+     *
+     * @param string $view filename of view
+     * @param array $data Data to pass to the view
+     * @return string
+     */
+    public function make($view, $data = [])
+    {
+
+    }
 }
